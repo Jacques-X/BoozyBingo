9825927c3cb10e8e0ac35c3663d06d78fd967209
JacquesX <jacques.xuereb.23@um.edu.mt>
2024-10-07 14:52:19 +0200

    Updated Code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cstdlib> // for rand() and srand()
-#include <ctime>   // for time()
+#include <cstdlib>  //for rand() and srand()
+#include <ctime>    //for time()
 
 class songs {
 private:
@@ -9,7 +9,7 @@ private:
     int songSheet[40][5][5];   //[People][Sheet Hight][Sheet Width]
 
 public:
-    // Getter for songTitles
+    //Getter for songTitles
     std::string getSongTitle(int index) const {
         if (index >= 0 && index < 100) {
             return songTitles[index];
@@ -17,47 +17,47 @@ public:
         return "";
     }
 
-    // Getter for songSelection
+    //Getter for songSelection
     int getSongSelectionValue(int person, int song) const {
         if (person >= 0 && person < 40 && song >= 0 && song < 25) {
             return songSelection[person][song];
         }
-        return -1; // Return -1 or another sentinel value to indicate an error
+        return -1; //Return -1 or another sentinel value to indicate an error
     }
 
-    // Getter for entire songSelection array
+    //Getter for entire songSelection array
     const int (*getSongSelection() const)[25] {
         return songSelection;
     }
 
-    // Getter for songSheet
+    //Getter for songSheet
     int getSongSheetValue(int person, int height, int width) const {
         if (person >= 0 && person < 40 && height >= 0 && height < 5 && width >= 0 && width < 5) {
             return songSheet[person][height][width];
         }
-        return -1; // Return -1 or another sentinel value to indicate an error
+        return -1; //Return -1 or another sentinel value to indicate an error
     }
 
-    // Getter for entire songSheet array
+    //Getter for entire songSheet array
     const int (*getSongSheet() const)[5][5] {
         return songSheet;
     }
 
-    // Setter for songTitles
+    //Setter for songTitles
     void setSongTitle(int index, const std::string &title) {
         if (index >= 0 && index < 100) {
             songTitles[index] = title;
         }
     }
 
-    // Setter for songSelection
+    //Setter for songSelection
     void setSongSelectionValue(int person, int song, int value) {
         if (person >= 0 && person < 40 && song >= 0 && song < 25) {
             songSelection[person][song] = value;
         }
     }
 
-    // Setter for entire songSelection array
+    //Setter for entire songSelection array
     void setSongSelection(const int newSongSelection[40][25]) {
         for (int i = 0; i < 40; ++i) {
             for (int j = 0; j < 25; ++j) {
@@ -66,7 +66,7 @@ public:
         }
     }
 
-    // Setter for songSheet
+    //Setter for songSheet
     void setSongSheetValue(int person, int height, int width, int value) {
         if (person >= 0 && person < 40 && height >= 0 && height < 5 && width >= 0 && width < 5) {
             songSheet[person][height][width] = value;
@@ -75,10 +75,10 @@ public:
 };
 
 void chooseSongs(int songSelection[40][25]) {
-    // Seed the random number generator with the current time
+    //Seed the random number generator with the current time
     srand(time(0));
 
-    // Fill the 2D array with random numbers from 0 to 24
+    //Fill the 2D array with random numbers from 0 to 24
     for (int i = 0; i < 40; ++i) {
         for (int j = 0; j < 25; ++j) {
             songSelection[i][j] = rand() % 25;
@@ -89,17 +89,17 @@ void chooseSongs(int songSelection[40][25]) {
 void randomiseSheet(int songSheet[40][5][5], const int songSelection[40][25]) {
     srand(time(0));
 
-    // Randomly distribute songs from the selectedSongs array to the songSheet array
+    //Randomly distribute songs from the selectedSongs array to the songSheet array
     for (int i = 0; i < 40; ++i) {
         for (int j = 0; j < 5; ++j) {
             for (int k = 0; k < 5; ++k) {
-                // Choose a random song from the selectedSongs array
+                //Choose a random song from the selectedSongs array
                 int randomSong = rand() % 25;
                 while (songSelection[i][randomSong] == -1) {
                     randomSong = rand() % 25;
                 }
 
-                // Place the song on the sheet
+                //Place the song on the sheet
                 songSheet[i][j][k] = songSelection[i][randomSong];
             }
         }
@@ -109,9 +109,9 @@ void randomiseSheet(int songSheet[40][5][5], const int songSelection[40][25]) {
 int main() {
     songs s;
 
-    // Choosing songs for each person
+    //Choosing songs for each person
     const int(*songSelection)[25] = s.getSongSelection();
-    // Create a non-const copy
+    //Create a non-const copy
     int songSelectionCopy[40][25];
     for (int i = 0; i < 40; ++i) {
         for (int j = 0; j < 25; ++j) {
@@ -120,13 +120,13 @@ int main() {
     }
     chooseSongs(songSelectionCopy);
 
-    // Create sheet for each person
+    //Create sheet for each person
     const int (*songSheet)[5][5] = s.getSongSheet();
-    // Create a non-const copy
+    //Create a non-const copy
     int songSheetCopy[40][5][5];
     for (int i = 0; i < 40; ++i) {
         for (int j = 0; j < 5; ++j) {
-            for (int k = 0; k < 5; ++k) {  // Added this loop to handle the third dimension
+            for (int k = 0; k < 5; ++k) {  
                 songSheetCopy[i][j][k] = (*songSheet)[j][k];
             }
         }
