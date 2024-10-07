79206bd881773ed40714cac61255329033c15e45
JacquesX <jacques.xuereb.23@um.edu.mt>
2024-10-07 19:18:31 +0200

    Build 1

---
+++ build/CMakeFiles/CMakeLists.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeLists.dir/main.cpp.o"
+  "CMakeFiles/CMakeLists.dir/main.cpp.o.d"
+  "libCMakeLists.a"
+  "libCMakeLists.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeLists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
